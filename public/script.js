658e1d8b6645ae6906f2837221ca56773ad9f9cb
Vinicius Maximiano <v.maximiano@hotmail.com>
2025-06-10 17:53:00 -0300

    Style and responsivity

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
+const closeMessage = document.getElementById("close-message");
+const message = document.getElementsByClassName("message")[0];
 
+closeMessage.addEventListener("click", () => {
+  message.style.display = "none";
+});
+
+setTimeout(() => {
+  message.style.display = "none";
+}, 4000);
